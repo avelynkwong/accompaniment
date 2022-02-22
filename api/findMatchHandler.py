4f79685517864cd1e2ce31c5a4f92b83bb131954
Avelyn Wong <avelynkwong@gmail.com>
2022-02-22 10:56:43 -0500

    removing app secrets

---
+++ api/findMatchHandler.py
@@ -0,0 +1,199 @@
+import os
+import spotipy
+import pandas as pd
+from spotipy.oauth2 import SpotifyClientCredentials
+from flask_restful import Api, Resource, reqparse
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier as knn
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from sklearn.preprocessing import MinMaxScaler
+from collections import Counter
+from os.path import join, dirname, realpath
+
+#? This takes in 3 playlists and generates a numpy array 
+#? to describe them based on spotify metadata (3 rows pertaining to playlist)
+#? call this function when a new user submits 3 playlists
+
+#? each playlist vector for other users is plotted in n-D space
+#? each vector is retrieved from the database (excluding your own playlists)
+#? find nearest 2 neighbours for each of your playlists (input into KNN model)
+#? you will have 6 potential matches, your top 2 soulmates should be the majority of your 6 matches
+#? retrieve thir Uid's send to eric so he can display their emails
+
+class find_match(Resource):
+    
+    def get(self):
+        
+        #? FUNCTION DEFINITIONS
+        #*given playlist links, extract features and upload to firebase dataset
+        FTR_NAMES = ['danceability', 'energy', 'speechiness', 'acousticness', 'instrumentalness', 'valence']
+        
+        def extract_features(playlist_links):
+            
+            #list containing the averaged ftr vectors for all 3 playlists
+            all_playlist_ftrs = []
+            
+            for playlist_link in playlist_links:
+                #each playlist is uniquely identifiable with a uniform resource identifier (URI)
+                #extract playlist URI
+                playlist_URI = playlist_link.split("/")[-1].split("?")[0]
+
+                #extract URI's of tracks in playlist
+                track_URIs = [x["track"]["uri"] for x in sp.playlist_tracks(playlist_URI)["items"]]
+                
+                #only keep the first 100 tracks (sp.audio_features can handle max 100 tracks)
+                #get audio features for all tracks in a playlist
+                ftrs = sp.audio_features(tracks=track_URIs)[:100]
+                
+                #turn the list of ftrs for tracks in the playlist into a pandas dataframe
+                #extract only the ftrs you want
+                #take the average along the columns
+                #convert to numpy
+                #append to all_playlist_ftrs
+                all_playlist_ftrs.append(pd.DataFrame(ftrs).loc[:, FTR_NAMES].mean(axis=0).to_numpy())
+            
+            #convert the list of playlist ftrs to a 2D numpy array (num playlists x num ftrs)
+            all_playlist_ftrs = np.array(all_playlist_ftrs)
+            return all_playlist_ftrs
+            
+        
+        #? OBTAINING USER'S PLAYLIST VECTOR
+        
+        #retrieving playlist links when get is called
+        parser = reqparse.RequestParser()
+        parser.add_argument('uid', type = str)
+        parser.add_argument('playlist1', type=str)
+        parser.add_argument('playlist2', type=str)
+        parser.add_argument('playlist3', type=str)
+        
+        args = parser.parse_args()
+        
+        uid = args.get('uid')
+        playlist1 = args.get('playlist1')
+        playlist2 = args.get('playlist2')
+        playlist3 = args.get('playlist3')
+        
+        playlists = [playlist1, playlist2, playlist3]
+        
+        #authentication to access general spotify data
+        client_credentials_manager = SpotifyClientCredentials() #<- insert credentials here
+        sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+        #obtain a 2D numpy array representing all 3 playlists (num playlists x num ftrs)
+        all_playlist_ftrs = extract_features(playlists)
+        
+        #? FIREBASE CREDENTIALS
+        #firebase credentials, only initalize once
+        if not firebase_admin._apps:
+            cred = credentials.Certificate({
+            #insert credentials here
+        })
+            firebase_admin.initialize_app(cred)
+        db = firestore.client()
+        
+        #?FINDING MATCH
+        
+        #get all the playlists vectors from other users in the database
+        docs = db.collection(u'song_metadata_collection').stream()
+
+        candidate_vec_dict = False #initialize to false, overwrite if there are docs in collection
+        
+        for doc in docs: #there is only one doc
+            candidate_vec_dict = doc.to_dict()
+            
+        
+        if candidate_vec_dict == False:
+            print('No other users')
+            return {
+                'topTwoMatches': [['None', -1], ['None', -1]], #there are no matches
+            }
+            
+        #candidates_vec_dict is a dictionary containing all the playlist vectors of other users
+        #the keys are the uids
+        #the values are another dictionary containing the 3 playlists
+        #the keys are playlist 1, playlist2, etc. and values are lists of playlist ftrs
+        labels = []
+        data = []
+        for uid_key in candidate_vec_dict:
+            if uid_key == uid: #skip over yourself if you're somehow already in the database
+                continue
+            curr_data = []
+            for playlist_key in candidate_vec_dict[uid_key]:
+                curr_data.append(candidate_vec_dict[uid_key][playlist_key])
+                labels.append(uid_key)
+            
+            data = data + curr_data
+        
+        mms = MinMaxScaler()
+        mms.fit(data)
+        scaled_data = mms.transform(data)
+
+        #plot playlist vectors for other users in n-d space
+        model = knn(n_neighbors=2)
+        model.fit(scaled_data, labels)
+        
+        #find top 2 matches for each of your playlists
+        top_matches = []
+        for i in range(len(all_playlist_ftrs)):
+            _, indices = model.kneighbors(all_playlist_ftrs[i].reshape(1,-1), n_neighbors = 2)
+            for j in range(len(indices[0])):
+                top_matches.append(labels[indices[0][j]])
+        
+        c = Counter(top_matches)
+        top2_matches = c.most_common(2) #a list where each entry is the uid + the number of times it occured in top 6
+        
+
+        #? UPLOADING YOUR USER'S PLAYLIST FEATURES TO FIREBASE
+        #name = "Debbie" #! need to get name from args 
+        for i in range(len(all_playlist_ftrs)):
+            data = {
+                uid:{
+                    "playlist" + str(i+1): list(all_playlist_ftrs[i])
+                }
+            }
+            # add the data to the map
+            db.collection(u'song_metadata_collection').document(u'user_vecs').set(data, merge=True)
+            
+        
+        
+        #!testing flask
+        return {
+        'topTwoMatches': top2_matches,
+        'message': "Did this work"
+        }
+    
+        
+        
+    def post(self):
+        print(self)
+        parser = reqparse.RequestParser()
+        parser.add_argument('type', type=str)
+        parser.add_argument('message', type=str)
+
+        args = parser.parse_args()
+
+        print(args)
+        # note, the post req from frontend needs to match the strings here (e.g. 'type and 'message')
+
+        request_type = args['type']
+        request_json = args['message']
+        # ret_status, ret_msg = ReturnData(request_type, request_json)
+        # currently just returning the req straight
+        ret_status = request_type
+        ret_msg = request_json
+
+        if ret_msg:
+            message = "Your Message Requested: {}".format(ret_msg)
+        else:
+            message = "No Msg"
+        
+        final_ret = {"status": "Success", "message": message}
+
+        return final_ret    
+            
+
+    
+
+    
